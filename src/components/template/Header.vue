ae100857faaa8ce1ea37834ef08be1dbd26bf75f
grandsmarquis <jdourlens@gmail.com>
2022-10-19 17:47:21 +0900

    browse deployment

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -126,27 +126,27 @@
           </PopoverButton>
         </div>
         <PopoverGroup as="nav" class="hidden space-x-10 md:flex">
-          <!--
+
           <Popover class="relative" v-slot="{ open }">
             <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-500', 'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2']">
-              <span>Solutions</span>
+              <span>Chains</span>
               <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />
             </PopoverButton>
 
             <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
               <PopoverPanel class="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
                 <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                  <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                    <a v-for="item in solutions" :key="item.name" :href="item.href" class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50">
+                  <!--<div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                    <a v-for="item in network" :key="item.name" :href="item.href" class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50">
                       <component :is="item.icon" class="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" />
                       <div class="ml-4">
                         <p class="text-base font-medium text-gray-900">{{ item.name }}</p>
                         <p class="mt-1 text-sm text-gray-500">{{ item.description }}</p>
                       </div>
                     </a>
-                  </div>
+                  </div>-->
                   <div class="space-y-6 bg-gray-50 px-5 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                    <div v-for="item in callsToAction" :key="item.name" class="flow-root">
+                    <div v-for="item in network" :key="item.name" class="flow-root">
                       <a :href="item.href" class="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100">
                         <component :is="item.icon" class="h-6 w-6 flex-shrink-0 text-gray-400" aria-hidden="true" />
                         <span class="ml-3">{{ item.name }}</span>
@@ -157,7 +157,6 @@
               </PopoverPanel>
             </transition>
           </Popover>
-          -->
 
           <!--
           <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Pricing</a>
@@ -407,12 +406,26 @@ import {
 } from "@heroicons/vue/24/outline";
 import { ChevronDownIcon } from "@heroicons/vue/20/solid";
 
-const solutions = [
+const network = [
   {
-    name: "Analytics",
+    name: "Mainnet",
     description:
-      "Get a better understanding of where your traffic is coming from.",
-    href: "#",
+      "",
+    href: "https://www.ethcmd.com",
+    icon: ChartBarIcon,
+  },
+  {
+    name: "Arbitrum",
+    description:
+      "",
+    href: "https://arbitrum.ethcmd.com",
+    icon: ChartBarIcon,
+  },
+  {
+    name: "Optimism",
+    description:
+      "",
+    href: "https://optimism.ethcmd.com",
     icon: ChartBarIcon,
   },
 ];
@@ -420,12 +433,19 @@ const callsToAction = [
   { name: "Watch Demo", href: "#", icon: PlayIcon },
   { name: "Contact Sales", href: "#", icon: PhoneIcon },
 ];
-const resources = [
+const solutions = [
   {
-    name: "Help Center",
+    name: "Mainnet",
     description:
       "Get all of your questions answered in our forums or contact support.",
-    href: "#",
+    href: "https://ethcmd.com",
+    icon: LifebuoyIcon,
+  },
+  {
+    name: "Arbitrum",
+    description:
+      "Get all of your questions answered in our forums or contact support.",
+    href: "https://arbitrum.ethcmd.com",
     icon: LifebuoyIcon,
   },
 ];
