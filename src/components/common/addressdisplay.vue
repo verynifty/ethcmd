1516f0c19c6bac08ccf558779254b44f2f030fbb
grandsmarquis <jdourlens@gmail.com>
2022-10-27 10:58:49 +0900

    transaction list for block

---
+++ src/components/common/addressdisplay.vue
@@ -0,0 +1,12 @@
+<template>
+    {{ format() }}
+</template>
+
+<script setup>
+    const props = defineProps(["value"]);
+
+    function format() {
+        return (props.value.substring(0, 6) + '...' + (props.value.slice(-6)))
+
+    }
+</script>
--- src/components/transaction/transactionlist.vue
+++ src/components/transaction/transactionlist.vue
@@ -3,7 +3,6 @@
     <div class="sm:flex sm:items-center">
       <div class="sm:flex-auto">
         <h1 class="text-xl font-semibold text-gray-900">Transactions</h1>
-        <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
       </div>
       <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
         <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add user</button>
@@ -19,23 +18,13 @@
                   <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Hash</th>
                   <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">From</th>
                   <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">To</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
-                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                    <span class="sr-only">Edit</span>
-                  </th>
                 </tr>
               </thead>
               <tbody class="bg-white">
                 <tr v-for="(tx, txId) in transactions" :key="tx.hash" :class="txId % 2 === 0 ? undefined : 'bg-gray-50'">
-                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ tx.hash }}  </td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ tx.hash }}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ tx.from }}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ tx.to }}</td>
-                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                    <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                      >Edit<span class="sr-only">, {{ tx.type }}</span></a
-                    >
-                  </td>
+                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"><TransactionHashDisplay :value="tx.hash" /></td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><AddressDisplay :value="tx.from" /></td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><AddressDisplay :value="tx.to" /></td>
                 </tr>
               </tbody>
             </table>
@@ -47,6 +36,8 @@
 </template>
 
 <script setup>
+import AddressDisplay from "@/components/common/addressdisplay.vue";
+import TransactionHashDisplay from "@/components/common/transactionhashdisplay.vue";
 
 const props = defineProps(["transactions"]);
 
