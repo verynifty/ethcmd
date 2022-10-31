2ff7e40a8cb52c1e80672808b937459eb2c8072c
grandsmarquis <jdourlens@gmail.com>
2022-10-31 14:14:34 +0900

    wip

--- src/components/transaction/lastblocks.vue
+++ src/components/transaction/lastblocks.vue
@@ -1,7 +1,5 @@
 <template>
-    <div>
-        dsfkdshfdsj
-    </div>
+  <div>dsfkdshfdsj</div>
 </template>
 
 <script setup>
@@ -11,24 +9,31 @@ const web3 = useWeb3Store();
 const ethers = await web3.getEthersAndConnect();
 
 let isLoading = true;
-let toBlock = await ethers.getBlockNumber("latest")
+let toBlock = await ethers.getBlockNumber("latest");
 let fromBlock = toBlock - 2;
-console.log(toBlock)
+console.log(toBlock);
 
-let blocksPromises = []
+let blocksPromises = [];
 for (let index = fromBlock; index < toBlock; index++) {
-   blocksPromises.push(ethers.getBlockWithTransactions(index))
+  blocksPromises.push(ethers.getBlockWithTransactions(index));
 }
-console.log(blocksPromises)
-const blocks = await Promise.all(blocksPromises)
-console.log(blocks)
+console.log(blocksPromises);
+const blocks = await Promise.all(blocksPromises);
+console.log(blocks);
 
-let targetContract = {}
+let targetContract = {};
+let targetFuncion = {};
 
 for (const b of blocks) {
-    for (const t of b.transactions) {
-        
-    }    
+  for (const t of b.transactions) {
+    if (t.data == null) continue;
+    let funcSelector = t.data.substring(0, 10).toLowerCase();
+    if (funcSelector.length != 10) continue;
+    if (targetContract[t.to] == null) {
+      targetContract[t.to] = {
+        caller: [],
+      };
+    }
+  }
 }
-
 </script>
