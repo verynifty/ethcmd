57d4449816ec7047ffd428f9cfcc8bbbbf3b8b0f
grandsmarquis <jdourlens@gmail.com>
2022-11-02 00:16:27 +0900

    test pblic labels

--- src/components/common/addressdisplay.vue
+++ src/components/common/addressdisplay.vue
@@ -5,8 +5,43 @@
 <script setup>
     const props = defineProps(["value"]);
 
-    function format() {
-        return (props.value.substring(0, 6) + '...' + (props.value.slice(-6)))
+    let known_addresses = {
+        "0xdac17f958d2ee523a2206206994597c13d831ec7": {
+            name: "Tether",
+            tags: ["ERC20", "StableCoin"]
+        },
+        "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": {
+            name: "WETH",
+            tags: ["ERC20"]
+        },
+        "0x7a250d5630b4cf539739df2c5dacb4c659f2488d": {
+            name: "UniswapV2Router",
+            tags: ["Uniswap"]
+        },
+        "0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45": {
+            name: "UniswapV3Router",
+            tags: ["Uniswap"]
+        },
+        "0x00000000006c3852cbef3e08e8df289169ede581": {
+            name: "Seaport",
+            tags: []
+        },
+        "0x1111111254fb6c44bac0bed2854e76f90643097d": {
+            name: "1inch",
+            tags: []
+        },
+        "0x283af0b28c62c092c9727f1ee09c02ca627eb7f5": {
+            name: "ENS",
+            tags: []
+        },
 
+    }
+
+    function format() {
+        if (known_addresses[props.value.toLowerCase()] != null)
+        {
+            return known_addresses[props.value.toLowerCase()].name;
+        }
+        return (props.value.substring(0, 6) + '...' + (props.value.slice(-6)))
     }
 </script>
