c2fa847cefd73d57c32582908d366f3ebac47b55
grandsmarquis <jdourlens@gmail.com>
2022-11-22 16:12:57 +0900

    mev view

---
+++ src/views/mev.vue
@@ -0,0 +1,15 @@
+
+
+<template>
+  <div class="h-full bg-gray-100 overflow-scroll">
+      hello
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+let relayscan = await axios.get("https://www.relayscan.io/api/stats")
+relayscan = relayscan.data;
+console.log(relayscan)
+</script>
