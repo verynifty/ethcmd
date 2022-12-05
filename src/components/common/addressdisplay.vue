39e41d0632377cb849cab0152a0dbc1f4f980979
grandsmarquis <jdourlens@gmail.com>
2022-12-06 08:33:31 +0900

    fix refs

--- src/components/common/addressdisplay.vue
+++ src/components/common/addressdisplay.vue
@@ -2,36 +2,36 @@
   <div class="w-min">
     <VTooltip>
       <router-link :to="{ name: 'addressinfos', params: { address: value } }"
-        >{{ format() }}
+        >{{ format }}
       </router-link>
-      <template   v-if="rolodethResult != null && rolodethResult.tags != null" #popper
-        >
-        {{value}}
+      <template
+        v-if="rolodethResult != null && rolodethResult.tags != null"
+        #popper
+      >
+        {{ value }}
         <div>
-        <span
-        
-          v-for="tag in rolodethResult.tags"
-          class="
-            text-xs
-            font-semibold
-            inline-block
-            py-1
-            px-2
-            uppercase
-            rounded
-            text-slate-600
-            bg-slate-200
-            uppercase
-            last:mr-0
-            mr-1
-          "
-        >
-          {{ tag }}
-        </span></div></template
+          <span
+            v-for="tag in rolodethResult.tags"
+            class="
+              text-xs
+              font-semibold
+              inline-block
+              py-1
+              px-2
+              uppercase
+              rounded
+              text-slate-600
+              bg-slate-200
+              uppercase
+              last:mr-0
+              mr-1
+            "
+          >
+            {{ tag }}
+          </span>
+        </div></template
       >
-       <template   v-else #popper
-        >{{value}}</template
-      >
+      <template v-else #popper>{{ value }}</template>
     </VTooltip>
   </div>
 </template>
@@ -39,39 +39,52 @@
 <script setup>
 import axios from "axios";
 const props = defineProps(["value"]);
-import { ref, watch, onMounted } from "vue";
+import { ref, watch, computed } from "vue";
 
+let loaded = ref(false);
 let rolodethResult = ref(null);
 
-async function loadAddress() {
+function loadAddress() {
   try {
-    let rolodeth = await axios.get(
-      "https://raw.githubusercontent.com/verynifty/RolodETH/main/data/" +
-        props.value.toLowerCase()
-    );
-    rolodethResult.value = rolodeth.data;
-  } catch (error) {}
+    let rolodeth = axios
+      .get(
+        "https://raw.githubusercontent.com/verynifty/RolodETH/main/data/" +
+          props.value.toLowerCase()
+      )
+      .then(function (rolodeth) {
+        rolodethResult.value = rolodeth.data;
+        loaded.value = true;
+      });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function format() {
-  if (rolodethResult != null) {
-    console.log(rolodethResult);
-  }
-  if (rolodethResult != null && rolodethResult.name != null) {
-    return rolodethResult.name;
-  }
-  if (rolodethResult != null && rolodethResult.ens != null) {
-    return rolodethResult.ens;
+const format = computed(() => {
+  if (loaded && rolodethResult != null && rolodethResult.value != null) {
+    console.log(
+      rolodethResult != null,
+      "COMPUTED",
+      props.value,
+      rolodethResult
+    );
+    if (rolodethResult.value.name != null) {
+      return rolodethResult.value.name;
+    }
+    if (rolodethResult.value.ens != null) {
+      return rolodethResult.value.ens;
+    }
   }
   return props.value.substring(0, 6) + "..." + props.value.slice(-6);
-}
+});
 
 loadAddress();
 
 watch(
   () => props.value,
   async function () {
-    rolodethResult.value = null;
+    rolodethResult = null;
+    loaded.value = false;
     loadAddress();
   }
 );
